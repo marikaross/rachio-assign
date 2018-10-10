c681cfb9bb5dee90884ee6c5585dd78d930388a4
Marika Ross <marika.ross@gmail.com>
2018-10-10 09:22:56 -0600

    Import Form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Form from '../Form/Form.js';
 require('./App.css');
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      zones: []
+    }
+  }
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <Form />
       </div>
       )
   }

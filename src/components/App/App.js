6692ab448566f917be628422aa60f4fab695be46
Marika Ross <marika.ross@gmail.com>
2018-10-10 14:05:32 -0600

    Write fetchZoneInfo method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Form from '../Form/Form.js';
+import ZoneContainer from '../ZoneContainer/ZoneContainer.js';
+import id from '../key.js';
 require('./App.css');
 
 
@@ -11,11 +13,26 @@ class App extends Component {
       zones: []
     }
   }
+
+  async componentDidMount() {
+    // const url = `https://api.rach.io/1/public/person/${id}`
+    await this.fetchZoneInfo()
+  }
+
+  async fetchZoneInfo() {
+    const url = `https://api.rach.io/1/public/person/${id}`
+    const response = await fetch(url)
+    const result = response.json()
+    const cleanedZones = cleanZones(result)
+    this.setState({zones: [...cleanedZones]})
+  }
+
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
         <Form />
+        <ZoneContainer zones={this.state.zones} />
       </div>
       )
   }
